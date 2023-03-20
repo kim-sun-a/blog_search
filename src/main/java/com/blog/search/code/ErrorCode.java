c4e05cc6bd629eee984efb0077e4612151ae029d
kim-sun-a <75839535+kim-sun-a@users.noreply.github.com>
2023-03-20 14:18:19 +0900

    error code 추가

--- src/main/java/com/blog/search/code/ErrorCode.java
+++ src/main/java/com/blog/search/code/ErrorCode.java
@@ -9,6 +9,9 @@ public enum ErrorCode {
     //400 BAD_REQUEST 잘못된 요청
     INVALID_PARAMETER(400, "파라미터 값을 확인해주세요."),
 
+    //404 NOT_FOUND 잘못된 리소스 접근
+    URL_NOT_FOUND(404, "잘못된 URL입니다."),
+
     //500 INTERNAL SERVER ERROR
     INTERNAL_SERVER_ERROR(500, "서버 에러입니다. 서버 팀에 연락주세요!");
 
